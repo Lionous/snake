c71f1d9322bb2bca3e955b78e2cb34eff1d1acc2
Henry <Ghreengate@gmail.com>
2024-07-17 23:33:33 -0500

    :fire: added  stage design

---
+++ src/main/java/com/onner/snake/Snake.java
@@ -0,0 +1,14 @@
+package com.onner.snake;
+
+import com.onner.form.Space;
+
+/**
+ * @author lionos
+ */
+public class Snake {
+
+    public static void main(String[] args) {
+        Space space = new Space();
+        space.setVisible(true);
+    }
+}
